7d037c27d44d487c40d438e883a09d0c2aca5b2b
Shaheer Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2020-06-06 16:05:08 +0500

    Fixed config values

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,6 +7,6 @@ export const typeOrmConfig: TypeOrmModuleOptions  = {
     username : 'postgres',
     password : 'Linkedlist22',
     database : 'taskmanagement',
-    entities : [__dirname + '/../**/*.entity.ts'],
+    entities: [__dirname + '/../**/*.entity.js'],
     synchronize : true,
 };
