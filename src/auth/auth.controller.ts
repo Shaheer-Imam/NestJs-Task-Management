2ecb122312edb227a9def2256fdd6938508c84a7
Shaheer Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2020-06-29 17:39:50 +0500

    controller redefined for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './auth-credentials.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authService: AuthService
+    ) {}
+
+    @Post('/signup')
+    signUp(@Body() authCredentialsDto: AuthCredentialsDto){
+        console.log(authCredentialsDto);
+    }
+}
  
