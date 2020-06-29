f98300189a55d245300ebc29d1112b504920a0ea
Shaheer Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2020-06-29 17:47:03 +0500

    Repository added

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,15 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./auth-credentials.dto";
 
 @EntityRepository()
 export class UserRepository extends Repository<User>{
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
+        const {username, password} = authCredentialsDto;
 
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        await user.save();
+    }
 }
