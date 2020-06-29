553457dd5c4346b35083c16aed6aa0c744c1d96e
Shaheer Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2020-06-29 17:45:47 +0500

    servies added for auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,8 @@ export class AuthService {
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
     ){}
+
+    async signUp(authCredentailsDto: AuthCredentialsDto): Promise<void>{
+        return this.userRepository.signUp(authCredentailsDto);
+    }
 }
