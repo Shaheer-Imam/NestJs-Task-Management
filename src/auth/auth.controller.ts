536ea96a92648a8ba68179435baffefc099e13f0
Shaheer Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2020-06-30 01:58:52 +0500

    Fixed database entry issue

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -9,8 +9,8 @@ export class AuthController {
     ) {}
 
     @Post('/signup')
-    signUp(@Body() authCredentialsDto: AuthCredentialsDto){
-        console.log(authCredentialsDto);
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto){
+        return this.authService.signUp(authCredentialsDto);
     }
 }
  
