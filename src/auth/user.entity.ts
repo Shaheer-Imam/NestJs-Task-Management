1b2fc48c8b46edf3ecd37fa496b14cee9aab9b0d
Shaheer Imam <42369778+Shaheer-Imam@users.noreply.github.com>
2020-07-25 02:31:52 +0500

    Entities added for user

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,9 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from "typeorm";
+import * as bcrupt from 'bcrypt';
+import { throws } from "assert";
 
 @Entity()
+@Unique(['username'])
 export class User extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
@@ -10,4 +13,12 @@ export class User extends BaseEntity {
 
     @Column()
     password: string;
+
+    @Column()
+    salt: string;
+
+    async validatePassword(password: string):Promise<boolean>{
+        const hash = await bcrupt.hash(password,this.salt);
+        return hash === this.password
+    }
 }
